ff970bff56b676f955a492c9c2757da7c36ad40d
lionswrath <caio.tonetti@gmail.com>
2016-06-09 20:27:08 -0300

    tree ready

--- enterprise_simulator.cpp
+++ enterprise_simulator.cpp
@@ -136,6 +136,7 @@ class Enterprise {
         //Passivas
         int getStock() { return stock; };
         float getPrice() { return productPrice; };
+        int getEmployees() { return numberOfEmployees; };
         building getTypeOfBuilding() { return typeOfBuilding; };
         investment getMarketResearch() { return marketResearch; };
         investment getPropaganda() { return propaganda; };
@@ -191,11 +192,13 @@ class Node {
 
         int getHeight() { return height; };
         vector<Enterprise> getPlayers() { return players; };
+        vector<Node> getPossiblePlays() { return possiblePlays; };
 
         void addPlayer(Enterprise enterprise) { players.push_back(enterprise); }
         void addPossiblePlay(Node node) { possiblePlays.push_back(node); };
-        int verifyPlayerTurn() { return height%players.size(); };
-        
+        int verifyPlayerTurn() { return height%players.size(); }; 
+        Enterprise getPlayer() { return players[verifyPlayerTurn()]; };
+
         //Criar heuristica
         float calculateHeuristic(Enterprise turnPlayer) {
             return 0.0f;
@@ -219,80 +222,64 @@ Node createRawPlay(Node * node) {
     return newNode;
 }
 
+void addMarketResearch(investment inv, Node * node, Node rawNode) {
+    if (node->getPlayer().getMarketResearch() != inv) {
+        rawNode.getPlayer().setMarketResearch(inv);
+        node->addPossiblePlay(rawNode);
+    }
+}
+
+void addPropaganda(investment inv, Node * node, Node rawNode) {
+    if (node->getPlayer().getPropaganda() != inv) {
+        rawNode.getPlayer().setPropaganda(inv);
+        node->addPossiblePlay(rawNode);
+    }
+}
+
+void addTraining(investment inv, Node * node, Node rawNode) {
+    if (node->getPlayer().getTraining() != inv) {
+        rawNode.getPlayer().setTraining(inv);
+        node->addPossiblePlay(rawNode);
+    }
+}
+
+
 void populatePossiblePlays(Node * node) {
     Node rawNode = createRawPlay(node);
 
     //Investment possibilities - Market Research
-    if (node->getMarketResearch != building::none)  {
-        rawNode->setMarketResearch(building::none); 
-        node->addPossiblePlay(rawNode); 
-        rawNode = createRawPlay(node);
-    }
-    if (node->getMarketResearch != building::low) { 
-        rawNode->setMarketResearch(building::low);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node);
-    }
-    if (node->getMarketResearch != bulding::medium) {
-        rawNode->setMarketResearch(building::medium);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
-    }
-    if (node->getMarketResearch != bulding::high) {
-        rawNode->setMarketResearch(building::high);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
-    }
+    addMarketResearch(investment::none, node, rawNode);
+    addMarketResearch(investment::low, node, rawNode);
+    addMarketResearch(investment::normal, node, rawNode);
+    addMarketResearch(investment::high, node, rawNode);
 
     //Investment possibilities - Propaganda
-    if (node->getPropaganda != building::none)  {
-        rawNode->setPropaganda(building::none); 
-        node->addPossiblePlay(rawNode); 
-        rawNode = createRawPlay(node);
-    }
-    if (node->getPropaganda != building::low) { 
-        rawNode->setPropaganda(building::low);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node);
-    }
-    if (node->getPropaganda != bulding::medium) {
-        rawNode->setPropaganda(building::medium);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
-    }
-    if (node->getPropaganda != bulding::high) {
-        rawNode->setPropaganda(building::high);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
-    }
+    addPropaganda(investment::none, node, rawNode);
+    addPropaganda(investment::low, node, rawNode);   
+    addPropaganda(investment::normal, node, rawNode);
+    addPropaganda(investment::high, node, rawNode);
 
     //Investment possibilities - Training
-    if (node->getTraining != building::none)  {
-        rawNode->setTraining(building::none); 
-        node->addPossiblePlay(rawNode); 
-        rawNode = createRawPlay(node);
-    }
-    if (node->getTraining != building::low) { 
-        rawNode->setTraining(building::low);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node);
-    }
-    if (node->getTraining != bulding::medium) {
-        rawNode->setTraining(building::medium);
-        node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
-    }
-    if (node->getTraining != bulding::high) {
-        rawNode->setTraining(building::high);
+    addTraining(investment::none, node, rawNode);
+    addTraining(investment::low, node, rawNode);   
+    addTraining(investment::normal, node, rawNode);
+    addTraining(investment::high, node, rawNode);
+
+    //Update building
+    if (node->getPlayer().getTypeOfBuilding() != building::large) {
+        rawNode.getPlayer().upgradeBuilding();
         node->addPossiblePlay(rawNode);
         rawNode = createRawPlay(node); 
     }
 
-    //Update building
-    if (node->getTypeOfBuilding() == bulding::large) {
-        rawNode->upgradeBuilding();
+    //Add employees
+    int numberOfEmployees = node->getPlayer().getEmployees();
+    if (numberOfEmployees < MAX_FUNC_SMALL || 
+        numberOfEmployees < MAX_FUNC_MEDIUM ||
+        numberOfEmployees < MAX_FUNC_LARGE) {
+        rawNode.getPlayer().addEmployee();
         node->addPossiblePlay(rawNode);
-        rawNode = createRawPlay(node); 
+        rawNode = createRawPlay(node);
     }
 }
 
@@ -311,4 +298,10 @@ int main() {
     cout << endl << CACCOM.calculatePorcentual(113, 318.153) << endl;
     cout << CACCOM.calculateCost() << endl;
     cout << CACCOM.calculateProfit(113, 318.153) << endl;
+
+    Node node(0, NULL);
+    node.addPlayer(CACCOM);
+
+    populatePossiblePlays(&node);
+    cout << node.getPossiblePlays().size() << endl;
 }
